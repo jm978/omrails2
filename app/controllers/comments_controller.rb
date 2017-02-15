3a8f0bcf89a788940159bb08c06f82600ece09c8
Jennifer Mitchell <beehappy@digitalhive.biz>
2017-02-15 08:15:50 -0600

    Preparing to add commenting

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,21 @@
+class CommentsController < ApplicationController
+  before_action :authenticate_user!
+
+  def create
+    post = Post.find_by(id: params[:id])
+    comment = post.comments.build(comment_params)
+    comment.user = current_user
+
+    if comment.save
+      flash[:notice] = "Comment has been created."
+      redirect_to post
+    else
+      flash[:alert] = "Comment has not been created."
+    end
+  end
+
+  private    
+    def comment_params
+      params.permit(:comment)
+    end
+end
